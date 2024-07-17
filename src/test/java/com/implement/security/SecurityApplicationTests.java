d90be7328c931b6ff780a607e32d811e224c1784
Haden0101 <ivqc_23@outlook.com>
2024-07-16 19:30:31 -0500

    Subo cambios de configuración inicial del proyecto

---
+++ src/test/java/com/implement/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.implement.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
